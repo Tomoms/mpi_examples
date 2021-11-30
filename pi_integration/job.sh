0d020668275972cb32a0eeafe6ec7b5a50f8c4a3
Tommaso Fonda <fondatommaso2@gmail.com>
2021-11-30 16:22:58 +0100

    job.sh: fix paths

--- pi_integration/job.sh
+++ pi_integration/job.sh
@@ -2,5 +2,5 @@
 #PBS -l nodes=1:ppn=4,walltime=0:10:00 -q dssc
 
 module load openmpi-4.1.1+gnu-9.3.0
-cd /u/dssc/tfonda/fast/hpc/pi_integration
+cd /u/dssc/tfonda/fast/hpc/git/pi_integration
 mpirun -np 4 --oversubscribe pi_integration ${n}
