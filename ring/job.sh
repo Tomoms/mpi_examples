ae34a53914f42ad448bf8f268a593abb3ec1c850
Tommaso Fonda <fondatommaso2@gmail.com>
2021-12-05 11:55:44 +0100

    ring: rewrite using virtual topologies

--- ring/job.sh
+++ ring/job.sh
@@ -2,5 +2,5 @@
 #PBS -l nodes=1:ppn=4,walltime=0:20:00 -q dssc
 
 module load openmpi
-cd /u/dssc/tfonda/fast/hpc/git/ring
+cd /u/dssc/tfonda/fast/hpc/git/ring_vt
 mpirun -np 4 --oversubscribe ring
--- ring/ring.c
+++ ring/ring.c
@@ -1,51 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mpi.h"
 
 int main(int argc, char *argv[])
 {
-    int rank, nproc;
+	int rank, nproc;
+	const int one[1] = {1};
+	int neighbors[2];
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Status status_add, status_subtract;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+	MPI_Comm ring_comm;
+	MPI_Status status_add, status_subtract;
 
-    const int right = (rank + 1) % nproc;
-    const int left = !rank ? nproc - 1 : rank - 1;
-    int payload_add = 0, payload_subtract = 0;
-    int received = 0;
+	int ret = MPI_Cart_create(MPI_COMM_WORLD, 1, &nproc, one, 1, &ring_comm);
+	if (ret) {
+		perror("MPI_Cart_create()");
+		MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+	}
 
-    if (!rank) {
-        int former_add, former_subtract;
-        for (int i = 0; i < nproc; i++) {
-            former_add = payload_add;
-            MPI_Recv(&payload_add, 1, MPI_INT, right, 10 * right, MPI_COMM_WORLD, &status_add);
-            received++;
-            MPI_Send(&former_add, 1, MPI_INT, left, 10 * rank, MPI_COMM_WORLD);
+	MPI_Cart_shift(ring_comm, 0, 1, &neighbors[0], &neighbors[1]);
+	MPI_Comm_rank(ring_comm, &rank);
+	int payload_add = 0, payload_subtract = 0;
+	int received = 0;
 
-            former_subtract = payload_subtract;
-            MPI_Recv(&payload_subtract, 1, MPI_INT, left, 10 * left, MPI_COMM_WORLD, &status_subtract);
-            received++;
-            MPI_Send(&former_subtract, 1, MPI_INT, right, 10 * rank, MPI_COMM_WORLD);
-        }
-    } else {
-        for (int i = 0; i < nproc; i++) {
-            payload_add += rank;
-            MPI_Send(&payload_add, 1, MPI_INT, left, 10 * rank, MPI_COMM_WORLD);
-            MPI_Recv(&payload_add, 1, MPI_INT, right, 10 * right, MPI_COMM_WORLD, &status_add);
-            received++;
+	if (!rank) {
+		int former_add, former_subtract;
+		for (int i = 0; i < nproc; i++) {
+			former_add = payload_add;
+			MPI_Recv(&payload_add, 1, MPI_INT, neighbors[1], 10 * neighbors[1], ring_comm, &status_add);
+			received++;
+			MPI_Send(&former_add, 1, MPI_INT, neighbors[0], 10 * rank, ring_comm);
 
-            payload_subtract -= rank;
-            MPI_Send(&payload_subtract, 1, MPI_INT, right, 10 * rank, MPI_COMM_WORLD);
-            MPI_Recv(&payload_subtract, 1, MPI_INT, left, 10 * left, MPI_COMM_WORLD, &status_subtract);
-            received++;
-        }
-    }
-    
-    printf("I am process %d and I have received %d messages.", rank, received);
-    printf("My final messages have got tag %d (from the right) and %d (from the left), and values %d (from the right) and %d (from the left)\n",
-           status_add.MPI_TAG, status_subtract.MPI_TAG, payload_add, payload_subtract);
+			former_subtract = payload_subtract;
+			MPI_Recv(&payload_subtract, 1, MPI_INT, neighbors[0], 10 * neighbors[0], ring_comm, &status_subtract);
+			received++;
+			MPI_Send(&former_subtract, 1, MPI_INT, neighbors[1], 10 * rank, ring_comm);
+		}
+	} else {
+		for (int i = 0; i < nproc; i++) {
+			payload_add += rank;
+			MPI_Send(&payload_add, 1, MPI_INT, neighbors[0], 10 * rank, ring_comm);
+			MPI_Recv(&payload_add, 1, MPI_INT, neighbors[1], 10 * neighbors[1], ring_comm, &status_add);
+			received++;
 
-    MPI_Finalize();
-    return 0;
+			payload_subtract -= rank;
+			MPI_Send(&payload_subtract, 1, MPI_INT, neighbors[1], 10 * rank, ring_comm);
+			MPI_Recv(&payload_subtract, 1, MPI_INT, neighbors[0], 10 * neighbors[0], ring_comm, &status_subtract);
+			received++;
+		}
+	}
+
+	printf("I am process %d and I have received %d messages. ", rank, received);
+	printf("My final messages have got tag %d (from the right) and %d (from the left), and values %d (from the right) and %d (from the left)\n",
+		status_add.MPI_TAG, status_subtract.MPI_TAG, payload_add, payload_subtract);
+
+	MPI_Finalize();
+	return 0;
 }
